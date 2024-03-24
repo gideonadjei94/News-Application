105e73be6ab3e22c4d731ffa533a4a5407879315
gideonadjei <gideonadjei94@gmail.com>
2024-03-24 03:42:03 -0700

    Setting up APIs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+
+const alanKey =
+  "451c57eec30e9385a287d29b8f88686d2e956eca572e1d8b807a3e2338fdd0dc/stage";
 function App() {
-  return <div className="App"></div>;
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+    });
+  }, []);
+  return (
+    <div className="App">
+      <h1>Alan AI News Application</h1>
+    </div>
+  );
 }
 
 export default App;
